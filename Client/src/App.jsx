c26fec8e64fdf41ec6293dfcf752d21dfc4154dd
bhardwajdheeraj <bhardwajdheeraj761@gmail.com>
2025-08-05 21:51:01 +0530

    add components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 export default function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <BrowserRouter>
+  {/* header */}
+  <Header/>
+  <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/signin" element={<SignIn/>} />
+      <Route path="/profile" element={<Profile/>} />
+  </Routes>
+  </BrowserRouter>
 }
---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ShieldIcon from '../assets/auth-logo.jpg'; // Correct path assuming structure
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white p-4 shadow-lg">
+      <div className="flex space-x-1">
+        
+        <img
+          src={ShieldIcon}
+          alt="Shield Icon"
+          className="w-6 h-6 rounded-sm"
+        />
+        <Link to='/'>
+        <h1 className="font-bold">Auth App</h1>
+        </Link>
+        
+      <div className="flex-grow flex justify-end">
+
+        <ul className='font-semibold flex space-x-4'>
+          <Link to='/'><li>Home</li></Link>
+          <Link to='about'><li>About</li></Link>
+          <Link to='signup'><li>Sign Up</li></Link>
+          <Link to='signin'><li>Sign In</li></Link>
+            
+            
+            
+            
+
+        </ul>
+      </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Header;
