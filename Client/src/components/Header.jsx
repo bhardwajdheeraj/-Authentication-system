9df0630a6981f96986f595ae28bd9501c331dd10
bhardwajdheeraj <bhardwajdheeraj761@gmail.com>
2025-08-12 22:36:10 +0530

    update header component

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ShieldIcon from '../assets/auth-logo.jpg'; // Correct path assuming structure
 import { Link } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+
 
 const Header = () => {
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white p-3 shadow-lg">
       <div className="flex space-x-1">
@@ -21,14 +24,14 @@ const Header = () => {
         <ul className='font-semibold flex space-x-4 text-black'>
           <Link to='/'><li>Home</li></Link>
           <Link to='about'><li>About</li></Link>
+        
           {/* <Link to='signup'><li>Sign Up</li></Link> */}
-          <Link to='signin'><li>Sign In</li></Link>
-            
-            
-            
-            
-
-        </ul>
+          <Link to='/profile'>
+          {currentUser ? 
+          (<img src={currentUser.profilePicture} alt="profile" className='h-7 w-7 rounded-full object-cover'/>):
+          ( <li>Sign In</li>)}
+         </Link>
+         </ul>
       </div>
         
       </div>
